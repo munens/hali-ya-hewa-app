ed51161767f005d573f6bec5e14b60be07ba6cd4
munens <kaumbuthom@yahoo.com>
2017-05-02 16:07:23 -0700

    Sec. 5, lec. 75: Table View delegate and datasource

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'hali-ya-hewa' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for hali-ya-hewa
+  pod 'Alamofire', '~> 4.4'
+
+  target 'hali-ya-hewaTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'hali-ya-hewaUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- hali-ya-hewa/WeatherVC.swift
+++ hali-ya-hewa/WeatherVC.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherVC: UIViewController {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImageView: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    /*
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    */
 
 }
 
